7e076f2c19ff939b16efa51ed85889e5e574f17a
Irshandy Aditya <irshandy.aditya@gmail.com>
2024-03-14 20:14:42 +0700

    Praktikum 2.1 Retrieving Single Models

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,16 +11,24 @@ class UserController extends Controller
     public function index()
     {
         // tambah data dengan Eloquent Model
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data); // menambahkan data ke tabel m_user
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data); // menambahkan data ke tabel m_user
 
         // coba akses user model
-        $user = UserModel::all(); // ambil semua data dari tabel m_user
+        // $user = UserModel::find(1); mengambil id 1 
+        // $user = UserModel::where('level_id', 1)->first(); mengambil 1 data teratas dgn level_id 1
+        // $user = UserModel::firstWhere('level_id', 1);
+        //$user = UserModel::findOr(1, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        $user = UserModel::findOr(20, ['username', 'nama'], function () {
+            abort(404);
+        });
         return view('user', ['data' => $user]);
     }
 }
