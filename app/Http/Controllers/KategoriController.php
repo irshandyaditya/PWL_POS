3f6c8c34c10af0cb581f1f6fe44ea9cf1993aba1
Irshandy Aditya <irshandy.aditya@gmail.com>
2024-03-20 09:35:21 +0700

    Praktikum 3 – Membuat form kemudian menyimpan data dalam database

--- app/Http/Controllers/KategoriController.php
+++ app/Http/Controllers/KategoriController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\DataTables\KategoriDataTable;
+use App\Models\KategoriModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -27,5 +28,19 @@ class KategoriController extends Controller
         // $data = DB::table('m_kategori')->get();
 
         return $dataTable->render('kategori.index');
+    }
+
+    public function create()
+    {
+        return view('kategori.create');
+    }
+    
+    public function store(Request $request)
+    {
+        KategoriModel::create([
+            'kategori_kode' => $request->kodeKategori,
+            'kategori_nama' => $request->namaKategori
+        ]);
+        return redirect('/kategori');
     }
 }
