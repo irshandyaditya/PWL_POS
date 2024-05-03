cb41bb20b83173ac1635e07d98868d88d3c58b74
Irshandy Aditya <irshandy.aditya@gmail.com>
2024-05-03 10:50:52 +0700

    Praktikum 2 – Membuat RESTful API Login

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class LoginController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        // set validation
+        $validator = Validator::make($request->all(),[
+            'username' => 'required',
+            'password' => 'required'
+        ]);
+
+        //if validation fail
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        //get credentials from request
+        $credentials = $request->only('username', 'password');
+
+        //if auth fail
+        if(!$token = auth()->guard('api')->attempt($credentials)){
+            return response()->json([
+                'success' => false,
+                'message' => 'Username atau Password salah',
+            ], 401);
+        }
+
+        //return JSON if auth success
+        return response()->json([
+            'success' => true,
+            'user' => auth()->guard('api')->user(),
+            'token' => $token
+        ], 200);
+    }
+}
