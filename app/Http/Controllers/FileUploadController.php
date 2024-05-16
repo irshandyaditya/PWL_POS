c61d2398565f155da8bd36f8820648d1bc88df86
Irshandy Aditya <irshandy.aditya@gmail.com>
2024-05-16 17:37:53 +0700

    Praktikum 4 - MEMINDAH FILE UPLOAD

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -38,6 +38,10 @@ class FileUploadController extends Controller
         $request->validate([
             'berkas' => 'required|file|image|max:500',
         ]);
-        echo $request->berkas->getClientOriginalName() . " lolos validasi";
+        $extFile = $request->berkas->getClientOriginalName();
+        $namaFile = 'web-' . time() . "." . $extFile;
+        $path = $request->berkas->storeAs('uploads', $namaFile);
+        echo "proses upload berhasil, data disimpan pada: $path";
+        // echo $request->berkas->getClientOriginalName() . " lolos validasi";
     }
 }
